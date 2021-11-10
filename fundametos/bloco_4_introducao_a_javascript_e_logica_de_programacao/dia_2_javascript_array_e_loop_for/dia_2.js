81147ffa88e5026e2bb07f1814b6483fb1c0836f
Kelly Cassiano <keukssie@hotmail.com>
2021-11-09 22:16:36 -0300

    conclusão do exercício 6

--- fundametos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_array_e_loop_for/dia_2.js
+++ fundametos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_array_e_loop_for/dia_2.js
@@ -48,7 +48,7 @@
 
   // 5 - Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
-  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
   let maior = 0
 
   for(let index = 0; index < numbers.length; index += 1){
@@ -56,7 +56,24 @@
        maior = numbers[index]
     }
   }
-  console.log(maior)
+  console.log(maior) */
+    // 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let impar = 0
+
+    for(let index = 0; index < numbers.length; index += 1){
+       if(numbers[index] % 2 != 0){
+          impar += 1;
+       }
+      }
+   
+    if(impar == 0){
+       console.log('nenhum valor ímpar encontrado');
+    } else {
+       console.log(impar)
+    }
+   
 
 
 
