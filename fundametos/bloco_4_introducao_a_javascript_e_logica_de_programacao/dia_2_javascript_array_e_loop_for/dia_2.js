3a259e424d49d9d000122a3f954ca69a61ec3f58
Kelly Cassiano <keukssie@hotmail.com>
2021-11-09 23:42:38 -0300

    ainda falta terminar

--- fundametos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_array_e_loop_for/dia_2.js
+++ fundametos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia_2_javascript_array_e_loop_for/dia_2.js
@@ -59,7 +59,7 @@
   console.log(maior) */
     // 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let impar = 0
 
     for(let index = 0; index < numbers.length; index += 1){
@@ -72,9 +72,14 @@
        console.log('nenhum valor ímpar encontrado');
     } else {
        console.log(impar)
-    }
-   
+    } */
 
+    // 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+    // ---
+
+     // 8 - Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+    
+     
 
 
  
